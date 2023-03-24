bd204708c9224beaa3ca10a17fec7a749be099b0
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2023-03-24 02:20:08 -0700

    periodic table : Modal Added

--- src/periodic-table/PeriodicTable.jsx
+++ src/periodic-table/PeriodicTable.jsx
@@ -2,13 +2,31 @@ import * as React from "react";
 import data from "./PeriodicTableJSON.json";
 import "./PeriodicTable.css";
 import { colorMap, colorMapFontColor, colorMapBackground } from "./ColorScheme";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 const PeriodicTable = () => {
+  const [elementObject, setElementObject] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+
+  function handleClickOpen(elementValue) {
+    setOpen(true);
+    setElementObject(elementValue);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="periodic-table">
       {data.elements.map((element) => (
         <div
-          onClick={() => alert(element.summary)}
+          onClick={(e) => handleClickOpen(element, e)}
           className="element"
           key={element.name}
           style={{
@@ -23,6 +41,23 @@ const PeriodicTable = () => {
           <small className="name">{element.name}</small>
         </div>
       ))}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        element={elementObject}
+      >
+        <DialogTitle id="alert-dialog-title">{elementObject.name}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {elementObject.summary}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
